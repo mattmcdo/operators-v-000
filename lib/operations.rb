2b38516e3fa3e239df93dd93f8504529b635e1b2
Matthew McDonald <matthew.mcd1031@gmail.com>
2018-02-03 15:14:53 +0000

    Done.

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,12 +1,14 @@
 def unsafe?(speed)
-
+	if speed > 60 || speed < 40
+		true
+	else
+		speed <=60 || speed >= 40
+		false
+	end
 end
 
 
 
 def not_safe?(speed)
-	
+	speed ? speed > 60 || speed < 40 : speed <=60 || speed >= 40
 end
-	
-
-
